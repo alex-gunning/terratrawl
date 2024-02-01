5cd9d86d9ca785f9243891a890faca628758dae5
Alex Gunning <dubbelodub1on1@gmail.com>
2024-02-01 22:57:52 +0100

    Add playwright toy. Add architecture diagram

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # terratrawl
 Monorepo for Terratrawl
+
+![Architecture](../system-diagrams/high-level-infra-guide.png)
--- terrascraper/src/app.ts
+++ terrascraper/src/app.ts
@@ -1,11 +1,21 @@
 import express, { Express, Request, Response } from 'express';
+import playwright from 'playwright';
+
 
 const app: Express = express();
 const hostname = process.env.HOSTNAME ?? 'localhost';
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
-app.get('/',(req: Request<string>, res: Response)=>{
-    res.status(200).send('Hello World!\n');
+app.get('/', async (req: Request<string>, res: Response)=>{
+  const browserType = 'chromium';
+  const browser = await playwright[browserType].launch()
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  await page.goto("https://amazon.com")
+  await page.screenshot({ path: 'page.png' })
+  await browser.close()
+
+  res.status(200).send('Visited amazon.com');
 });
 
 app.listen(port, hostname, () => {
