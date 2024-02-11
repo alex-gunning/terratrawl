ec59f0f6fae31f500d5d242c4981f6ceca9154a2
Alex Gunning <dubbelodub1on1@gmail.com>
2024-02-11 01:52:58 +0100

    Make terrascraper a bit more robust. Scrape additional data.

--- terrascraper/src/app.ts
+++ terrascraper/src/app.ts
@@ -26,18 +26,31 @@ app.use(express.json());
 app.post('/funda', async (req: Request<FundaRequest>, res: Response)=>{
   const fundaUrl = req.body.url
 
+
   const browserType = 'firefox';
   const browser = await playwright[browserType].launch()
   const context = await browser.newContext()
   const page = await context.newPage()
   await page.goto(fundaUrl)
+
   /*
-   * These should all be parallelized or pulled and the resulting DOM queried.
+   * Pull the various items out of the DOM.
    */
+  const isToHire: boolean = new URL(fundaUrl).pathname.split('/')[1] === 'huur';
   const streetAddress = await page.$eval('.object-header__container .object-header__title', title => { return title.innerHTML })
   const postalCode = await page.$eval('.object-header__container .object-header__subtitle', code => { return code.textContent?.split('\n')[0] })
-  const meters = await page.$eval('.object-header__container section > ul > li:nth-child(1) > span:nth-child(2)', meters => { return meters.textContent?.replace(/\D/g, '') })
-  const bedrooms = await page.$eval('.object-header__container section > ul > li:nth-child(2) > span:nth-child(2)', meters => { return meters.textContent })
+
+  const bedrooms = await page.getByRole('listitem')
+    .filter({ hasText: 'slaapkamers' })
+    .allTextContents()
+    .then(rawData => rawData[0].replace(/\D/g, ''))
+  const meters = await page.getByRole('listitem')
+    .filter({ hasText: 'wonen' })
+    .allTextContents()
+    .then(rawData => rawData[0].replace(/\D/g, ''))
+
+  const primaryPrice = await page.$$eval('.object-header__price', price => { return price[0]?.textContent?.replace(/\D/g, '') });
+  const secondaryPrice = await page.$$eval('.object-header__secondary-price', price => { return price[0]?.textContent?.replace(/\D/g, '') });
 
   await browser.close()
 
@@ -45,7 +58,10 @@ app.post('/funda', async (req: Request<FundaRequest>, res: Response)=>{
     streetAddress,
     postalCode,
     meters,
-    bedrooms
+    bedrooms,
+    isToHire,
+    primaryPrice,
+    secondaryPrice,
   }
 
   res.status(200).send(result);
